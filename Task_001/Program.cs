5f5733b67f5747b4fcd10954bcaa304be72e1aee
unknown <C:\Users\GMS\AppData\Roaming\The Bat!>
2023-12-22 17:13:31 +0700

    Задание 1 доделал

--- Task_001/Program.cs
+++ Task_001/Program.cs
@@ -4,6 +4,8 @@
 //в промежутке от M до N. Использовать рекурсию, не
 //использовать циклы.
 
+int M, N;
+
 int ReadInt(string str)
 {
     System.Console.Write(str);
@@ -17,6 +19,34 @@ void PrintNumbers(int M, int N)
     PrintNumbers(M + 1, N);
 }
 
-int M = ReadInt("Введите меньшее число: ");
-int N = ReadInt("Введите большее число: ");
-PrintNumbers(M, N);
+void EnterNum()
+{
+    M = ReadInt("Введите меньшее число: ");
+    N = ReadInt("Введите большее число: ");
+    CheckNum(M, N);
+}
+
+void CheckNum(int M, int N)
+{
+
+    if (M < N)
+        PrintNumbers(M, N);
+    else if (M > N)
+    {
+        System.Console.WriteLine("Числа поменяли местами из-за того, что M больше N");
+        System.Console.WriteLine();
+        int p = M;
+        M = N;
+        N = p;
+        PrintNumbers(M, N);
+    }
+    else
+    {
+        System.Console.WriteLine("Вы ввели одинаковые числа, повторите попытку");
+        System.Console.WriteLine();
+        EnterNum();
+    }
+
+}
+
+
