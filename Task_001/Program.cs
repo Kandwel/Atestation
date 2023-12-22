9df6e88505468b1259d395fa587fe5eb0747fda5
unknown <C:\Users\GMS\AppData\Roaming\The Bat!>
2023-12-22 17:01:23 +0700

    Задание 1

--- Task_001/Program.cs
+++ Task_001/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 1: 
+//Задайте значения M и N. Напишите
+//программу, которая выведет все натуральные числа
+//в промежутке от M до N. Использовать рекурсию, не
+//использовать циклы.
+
+int ReadInt(string str)
+{
+    System.Console.Write(str);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintNumbers(int M, int N)
+{
+    if (M > N) return;
+    System.Console.Write(M + " ");
+    PrintNumbers(M + 1, N);
+}
+
+int M = ReadInt("Введите меньшее число: ");
+int N = ReadInt("Введите большее число: ");
+PrintNumbers(M, N);
